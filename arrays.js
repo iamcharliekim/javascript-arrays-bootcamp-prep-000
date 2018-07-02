ad5e3e6ad7a312e19d0004271bec35a6d6925f67
Charles Kim <cwkim3@gmail.com>
2018-07-02 22:52:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,4 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function chocolateBars(){
   return "snickers", "hundred grand", "kitkat", "skittles"
 }
-function addElementToBeginningOfArray 
+function addElementToBeginningOfArray(array, )
