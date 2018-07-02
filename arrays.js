36737873764df7946d97fae5845004be030471b1
Charles Kim <cwkim3@gmail.com>
2018-07-02 23:08:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,6 @@ function chocolateBars(){
 }
 function addElementToBeginningOfArray(array, element){
     return [element, ...array]}
+function destructivelyAddElementToBeginningOfArray(array, element){
+    return
+}
