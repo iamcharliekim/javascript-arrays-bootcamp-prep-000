e97cc0bf3f102a20ca255f6c11b0e6975e3b3efd
Charles Kim <cwkim3@gmail.com>
2018-07-02 22:49:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,4 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function chocolateBars()
+function chocolateBars(){
+  return "snickers", "hundred grand", "kitkat", "skittles"
+}
