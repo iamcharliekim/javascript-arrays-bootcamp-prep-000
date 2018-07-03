9ab7edf45476c65fda6966a0d0c47655042e842a
Charles Kim <cwkim3@gmail.com>
2018-07-03 00:05:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,5 @@ function addElementToBeginningOfArray(array, element){
     return [element, ...array]}
 function destructivelyAddElementToBeginningOfArray(array, element){
     array.unshift(element)
-    return array
-}
+    return array}
 
