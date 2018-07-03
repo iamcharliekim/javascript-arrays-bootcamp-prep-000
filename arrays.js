a860470b525aacb8fd3f60dbe4f5470933128786
Charles Kim <cwkim3@gmail.com>
2018-07-03 00:20:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,4 +16,5 @@ function accessElementInArray(array, index){return array[index]}
 function destructivelyRemoveElementFromBeginningOfArray(array){
     array.shift()
     return array}
-function removeElementFromBeginningOfArray(array){return array.slice(1)
+function removeElementFromBeginningOfArray(array){
+    return array.slice(1)}
