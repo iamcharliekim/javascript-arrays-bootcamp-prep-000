d000aaf7cad91b73c36b844a8dd7753a55d2fe46
Charles Kim <cwkim3@gmail.com>
2018-07-03 00:21:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,8 +13,8 @@ function destructivelyAddElementToEndOfArray(array, element){
     array.push(element)
     return array}
 function accessElementInArray(array, index){return array[index]}
-function destructivelyRemoveElementFromEndOfArray(array){
+function destructivelyRemoveElementFromBeginningOfArray(array){
     array.shift()
     return array}
-function removeElementFromEndOfArray(array){
+function removeElementFromBeginningOfArray(array){
     return array.slice(1)}
